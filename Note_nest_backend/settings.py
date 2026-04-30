268dc62db51b94c8b32341025406a935e49e7ae7
Mahdi767 <mehedi49891@gmail.com>
2026-04-30 22:32:17 +0600

    switched to port 465 SSL

--- Note_nest_backend/settings.py
+++ Note_nest_backend/settings.py
@@ -155,8 +155,8 @@ SPECTACULAR_SETTINGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='NoteNest <noreply@notenestmu.me>')
